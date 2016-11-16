ff0eadc428f50afb5137ae1522565504db4be074
Luv Agarwal <luvagarwal.q@gmail.com>
2016-11-16 19:03:18 +0530

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TerroristPlotDetection',
+      version='1.0',
+      description='Implementation of paper "Computing Optimal Monitoring Strategy for Detecting Terrorist Plots"',
+      author='',
+      author_email='',
+      url='https://github.com/luviiit/TerroristPlotDetection',
+     )
