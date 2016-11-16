a1d442375ba9525541992b15eb734cdf6284a888
NPSDC <noorpratap@gmail.com>
2016-11-16 23:32:41 +0530

    Added sample graph images and implementation of scale free

--- TerroristPlotDetection/inputs.py
+++ TerroristPlotDetection/inputs.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
 " Contains various input trees "
-
-import networkx
+import networkx as nt
 
 class BarabasiAlbert():
-    pass
+    def __init__(self, n, m, seed):
+		self.n = n
+		self.m = m
+		self.seed = seed
+        self.G = nt.barabasi_albert_graph(self.n, self.m, self.seed)
+    
+    def draw_plot(self):
+        nt.draw(self.G)
+        plt.show()
 
 class RandomTree():
     pass
 
 class ErdosRenyi():
     pass
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
